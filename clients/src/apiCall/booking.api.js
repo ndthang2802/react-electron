2b5c4fde59a4d3b0eee2102c6d5e1c8787aaf1e1
Duc <ndthang2802@gmail.com>
2021-06-22 18:20:10 +0700

    update api call in booking page

--- clients/src/apiCall/booking.api.js
+++ clients/src/apiCall/booking.api.js
@@ -1,14 +1,23 @@
 class BookingApiCall{
     getById(id){
-        url = 'url'+`?id=${id}`
+        var url = 'url'+`?id=${id}`
         return fetch(url)
     }
     update(data){
 
     }
     getAvailable(){
-        url = 'url'+'status=available'
+        var url = 'url'+'status=available'
         return fetch(url)
+    }
+    getAll(){
+        return fetch('https://fake-api-nnn.herokuapp.com/api/roomrentals').then(data=>data.json())
+    }
+    getClientInfo(idrental){
+        return fetch(`https://fake-api-nnn.herokuapp.com/api/roomrentals/${idrental}/clients`).then(data=>data.json())
+    }
+    getRoomInfo(idrental){
+        return fetch(`https://fake-api-nnn.herokuapp.com/api/roomrentals/${idrental}/rooms`).then(data=>data.json())
     }
 }
 export default new BookingApiCall()
