820b69ad05b359075884120ecd3e4c59a6dc9e9c
Duc <ndthang2802@gmail.com>
2021-07-05 00:59:23 +0700

    complete add booking

--- clients/src/components/dialog/addBooking.js
+++ clients/src/components/dialog/addBooking.js
@@ -1,14 +1,8 @@
 import React,{ useEffect, useReducer, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import {TextField,Grid,makeStyles,Box,Table,TableHead,TableBody,TableRow,TableCell } from '@material-ui/core';
+import {TextField,Grid,makeStyles,Box,Table,TableHead,TableBody,TableRow,TableCell,Button,Dialog,DialogActions,DialogTitle,DialogContent,Slide } from '@material-ui/core';
 import {CloseOutlined,MonetizationOnOutlined,BackspaceOutlined} from '@material-ui/icons';
 import DatePickers from './DatePickers';
-import { ValidateAddBooking,hasError } from '../function/validate.addBooking';
+import { ValidateAddBooking,hasError } from '../function/validate.Booking';
 import ClientApiCall from '../../apiCall/client.api'
 import BookingApiCall from '../../apiCall/booking.api';
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -81,6 +75,7 @@ export default function AddBookings(props) {
     start_at: getDay(),
     check_out_at: getDay()
   }
+  const [error,setError] = useState({phone: '',name:'',identify:'',email:'',start_at:'',check_out_at:''})
   const [formInput, setFormInput] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
     initialState
@@ -90,16 +85,17 @@ export default function AddBookings(props) {
     setFormInput({email:''})
     setFormInput({phone:''})
     setFormInput({identify:''})
+    setError({phone: '',name:'',identify:'',email:'',start_at:'',check_out_at:''})
+
   }
   useEffect(()=>{
     clearAll()
   },[open,room_booked])
-  const [error,setError] = useState({phone: '',name:'',identify:'',email:'',start_at:'',check_out_at:''})
   const handleSubmit = async evt => {
     evt.preventDefault();
     const data = {formInput}
-    let E = ValidateAddBooking(data)
-    if (E.length) setError({...error,[E.key]:E.value})
+    let E = ValidateAddBooking(data.formInput)
+    if (Object.keys(E).length) setError({...error,[E.key]:E.value})
     else{
       if(!hasError(error)){
         var res = await BookingApiCall.addBooking(data.formInput)
@@ -162,129 +158,82 @@ export default function AddBookings(props) {
   
   return (
     <div>
-      <Dialog
-        maxWidth='md'
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-      >
+      <Dialog maxWidth='md' open={open} TransitionComponent={Transition} keepMounted onClose={handleClose} aria-labelledby="alert-dialog-slide-title" aria-describedby="alert-dialog-slide-description">
             <DialogTitle id="alert-dialog-slide-title">
               <Box className={classes.dialogTitle}>
-              <b>Add Bookings</b>
-              <Button variant="outlined" color="secondary" onClick={clearAll} >Clear&nbsp;&nbsp;<BackspaceOutlined/></Button>
+                <b>Add Bookings</b>
+                <Button variant="outlined" color="secondary" onClick={clearAll} >Clear&nbsp;&nbsp;<BackspaceOutlined/></Button>
               </Box>
             </DialogTitle>
-            <DialogContent dividers={true} className={classes.dialogContent} >
-            <form onSubmit={handleSubmit} className={classes.form} >
-              <Grid container spacing={5}>
-                <Grid container item xs={12} sm={6}>
-                  <TextField
-                    error ={error.phone !== '' ? true : false}
-                    label="Phone"
-                    id="margin-normal"
-                    name="phone"
-                    value={formInput.phone}
-                    className={classes.textField}
-                    onChange={handleInput}
-                    helperText={error.phone}
-                  />
-                </Grid>
-                <Grid container item xs={12} sm={6}>
-                  <TextField
-                    error={error.email !== '' ? true : false}
-                    label="Email"
-                    id="margin-normal"
-                    name="email"
-                    value={formInput.email}
-                    className={classes.textField}
-                    onChange={handleInput}
-                    helperText={error.email}
-                  />
-                </Grid>
-                <Grid container item xs={12} sm={6}>
-                  <TextField
-                    error={error.name !== '' ? true : false}
-                    label="Name"
-                    id="margin-normal"
-                    name="name"
-                    value={formInput.name}
-                    className={classes.textField}
-                    onChange={handleInput}
-                    helperText={error.name}
-                  />
-                </Grid>
-                <Grid container item xs={12} sm={6}>
-                  <TextField
-                    error={error.identify !== '' ? true : false}
-                    label="Identify"
-                    id="margin-normal"
-                    name="identify"
-                    value={formInput.identify}
-                    className={classes.textField}
-                    onChange={handleInput}
-                    helperText={error.identify}
-                  />
-                </Grid>
-                <Grid container item xs={12} sm={6}>
-                  <DatePickers label={'Check in'} name='start_at' error={error.start_at} handleChoose={handleChoose} selectedDate={formInput.start_at}/>
-                </Grid>
-                <Grid container item xs={12} sm={6}>
-                  <DatePickers label={'Check out'} name='check_out_at' error={error.check_out_at} handleChoose={handleChoose} selectedDate={formInput.check_out_at} />
-                </Grid>
-                <Grid container item xs={12} sm={11}>
-                  <Table>
-                      <TableHead>
-                          <TableRow>
-                          <TableCell><b>Category</b></TableCell>
-                            <TableCell><b>Room</b></TableCell>
-                            <TableCell><b>Floor</b></TableCell>
-                            <TableCell><b>Price</b></TableCell>
-                          </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {
-                          roomSelected.map((row,index) => {
-                            return (
-                              <TableRow key={index}>
-                                <TableCell>{row.Category}</TableCell>
-                                <TableCell>{row.Number}</TableCell>
-                                <TableCell>{row.Floor}</TableCell>
-                                <TableCell>{row.Price}</TableCell>
+              <DialogContent dividers={true} className={classes.dialogContent} >
+                <form onSubmit={handleSubmit} className={classes.form} >
+                  <Grid container spacing={5}>
+                    <Grid container item xs={12} sm={6}>
+                      <TextField error ={error.phone !== '' ? true : false}
+                        label="Phone" id="margin-normal" name="phone"
+                        value={formInput.phone} className={classes.textField}
+                        onChange={handleInput} helperText={error.phone}
+                      />
+                    </Grid>
+                    <Grid container item xs={12} sm={6}>
+                      <TextField error={error.email !== '' ? true : false}
+                        label="Email" id="margin-normal" name="email"
+                        value={formInput.email} className={classes.textField}
+                        onChange={handleInput} helperText={error.email}
+                      />
+                    </Grid>
+                    <Grid container item xs={12} sm={6}>
+                      <TextField error={error.name !== '' ? true : false}
+                        label="Name" id="margin-normal" name="name" value={formInput.name}
+                        className={classes.textField} onChange={handleInput} helperText={error.name}
+                      />
+                    </Grid>
+                    <Grid container item xs={12} sm={6}>
+                      <TextField error={error.identify !== '' ? true : false}
+                        label="Identify" id="margin-normal" name="identify"
+                        value={formInput.identify} className={classes.textField}
+                        onChange={handleInput} helperText={error.identify}
+                      />
+                    </Grid>
+                    <Grid container item xs={12} sm={6}>
+                      <DatePickers label={'Check in'} name='start_at' error={error.start_at} handleChoose={handleChoose} selectedDate={formInput.start_at}/>
+                    </Grid>
+                    <Grid container item xs={12} sm={6}>
+                      <DatePickers label={'Check out'} name='check_out_at' error={error.check_out_at} handleChoose={handleChoose} selectedDate={formInput.check_out_at} />
+                    </Grid>
+                    <Grid container item xs={12} sm={11}>
+                      <Table>
+                          <TableHead>
+                              <TableRow>
+                                <TableCell><b>Category</b></TableCell><TableCell><b>Room</b></TableCell><TableCell><b>Floor</b></TableCell><TableCell><b>Price</b></TableCell>
                               </TableRow>
-                            )
-                          })
-                        }
-                      </TableBody>
-                  </Table>
-                </Grid>
-              </Grid>
-            </form>
-            <Box></Box>
-            </DialogContent>
+                          </TableHead>
+                          <TableBody>
+                            {
+                              roomSelected.map((row,index) => {
+                                return (
+                                  <TableRow key={index}>
+                                    <TableCell>{row.Category}</TableCell><TableCell>{row.Number}</TableCell><TableCell>{row.Floor}</TableCell><TableCell>{row.Price}</TableCell>
+                                  </TableRow>
+                                )
+                              })
+                            }
+                          </TableBody>
+                      </Table>
+                    </Grid>
+                  </Grid>
+                </form>
+              </DialogContent>
         <DialogActions>
-        <Grid container item xs={12} >
+            <Grid container item xs={12} >
               <Box display='flex' justifyContent='flex-end' width='100%'>
-                <Button
-                  type="button"
-                variant="contained"
-                color="secondary"
-                className={classes.button}
-                onClick={handleClose}
-                >
-                Close <CloseOutlined className={classes.rightIcon} />
+                <Button type="button" variant="contained" color="secondary" className={classes.button} onClick={handleClose} > 
+                  Close <CloseOutlined className={classes.rightIcon} />
                 </Button>
-                <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                onClick={handleSubmit}
-                >
-                Book <MonetizationOnOutlined className={classes.rightIcon}/>
-              </Button>
+                
+                <Button type="submit" variant="contained"  color="primary" className={classes.button} onClick={handleSubmit} >
+                  Book <MonetizationOnOutlined className={classes.rightIcon}/>
+                </Button>
               </Box>
             </Grid>
         </DialogActions>
