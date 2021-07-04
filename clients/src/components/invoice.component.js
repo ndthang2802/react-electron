a8e60677857fa6f0cf470fc892d95729c6236fed
Duc <ndthang2802@gmail.com>
2021-07-05 01:01:29 +0700

    start build invoice component

--- clients/src/components/invoice.component.js
+++ clients/src/components/invoice.component.js
@@ -1,106 +1,62 @@
 import React,{useState}  from 'react';
 import EnhancedTable from './table/Table'
 import {InvoiceSideBar} from './sidebar.component'
-import { Grid } from '@material-ui/core';
-const data = [
-    {
-      "id": 0,
-      "published": "2016-02-27T02:19:04 -07:00",
-      "staff": "Fields Shepard",
-      "start": "2014-01-13T03:52:35 -07:00",
-      "checkout": "2015-10-23T05:44:11 -07:00",
-      "price": 687691,
-      "paid": true,
-      "Payee": 
-        {
-          "name": "Ratliff Witt",
-          "email": "ratliffwitt@envire.com",
-          "phone": "+1 (872) 415-2603",
-          "indentify": 828661
-        }
-      
-    },
-    {
-      "id": 1,
-      "published": "2015-04-04T02:30:23 -07:00",
-      "staff": "Lawson Fletcher",
-      "start": "2020-11-14T12:09:00 -07:00",
-      "checkout": "2014-06-29T01:44:55 -07:00",
-      "price": 550418,
-      "paid": true,
-      "Payee": 
-        {
-          "name": "Leanne Noble",
-          "email": "leannenoble@envire.com",
-          "phone": "+1 (887) 414-3961",
-          "indentify": 778350
-        }
-      
-    },
-    {
-      "id": 2,
-      "published": "2018-07-31T12:56:18 -07:00",
-      "staff": "Avila Best",
-      "start": "2018-01-01T07:27:32 -07:00",
-      "checkout": "2014-11-29T10:09:04 -07:00",
-      "price": 229915,
-      "paid": false,
-      "Payee": 
-        {
-          "name": "Eddie Carroll",
-          "email": "eddiecarroll@envire.com",
-          "phone": "+1 (911) 594-2098",
-          "indentify": 217910
-        }
-      
-    },
-    {
-      "id": 3,
-      "published": "2015-10-17T02:59:53 -07:00",
-      "staff": "Zimmerman Workman",
-      "start": "2016-03-14T10:50:24 -07:00",
-      "checkout": "2015-06-08T03:38:19 -07:00",
-      "price": 881620,
-      "paid": false,
-      "Payee": 
-        {
-          "name": "Houston Goodwin",
-          "email": "houstongoodwin@envire.com",
-          "phone": "+1 (806) 432-2980",
-          "indentify": 737116
-        }
-      
-    },
-    {
-      "id": 4,
-      "published": "2015-01-24T06:54:26 -07:00",
-      "staff": "Jarvis Delgado",
-      "start": "2019-11-27T10:40:31 -07:00",
-      "checkout": "2015-04-25T11:05:51 -07:00",
-      "price": 403240,
-      "paid": false,
-      "Payee": 
-        {
-          "name": "Roseann Sanders",
-          "email": "roseannsanders@envire.com",
-          "phone": "+1 (945) 508-2596",
-          "indentify": 285368
-        }
-      
-    }
-  ]
-
+import { Grid,Typography,makeStyles,TextField,Box,Button,Divider } from '@material-ui/core';
+import {FindInPageOutlined} from '@material-ui/icons';
+const useStyles = makeStyles(theme => ({
+  center:{
+    display: 'flex',
+    alignItems:'center'
+  },
+  rowdisplay:{
+    display:'flex',
+    flexDirection:'row',
+    alignContent:'center',
+    gap:'3rem',
+    boxShadow:'0 2px 1px -1px gray',
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1)
+  },
+  button: {
+    margin: theme.spacing(1),
+    backgroundColor : '#98E6FC',
+  },
+}));
 export default function Invoice(){
-    const Headers = ['id','published','price','paid']
-    const [selected, setSelected] = useState([]);
+    const classes = useStyles()
+    const [phone,setPhone] = useState('')
+    const [error,setError] = useState()
+    const phoneChange = (e)=>{
+      setPhone(e.target.value)
+    }
+    const getBill = () => {
+      if (phone.length != 10)  setError('Invalid phone')
+    }
     return (
-        <React.Fragment>
-          <Grid container item xs={4} sm={2}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={3}>
             <InvoiceSideBar  />
           </Grid>
-          <Grid container item xs={12} sm={10}>
-            <EnhancedTable data={data} Headers={Headers} tableName='Invoices' selected={selected} setSelected={setSelected} ></EnhancedTable>
+          <Grid container item xs={12} sm={9}>
+            <Box width='100%'>
+              <Grid container spacing={5}>
+                  <Grid item xs={12} sm={12}><b>Kiểm tra thông tin hóa đơn</b></Grid>
+                  <Grid item xs={12} sm={1}></Grid>
+                  <Grid item xs={12} sm={10} className={classes.rowdisplay}>
+                    <Typography component='div' className={classes.center} >Nhập số điện thoại: </Typography>
+                    <TextField id="outlined-basic" label="Phone" onChange={phoneChange} />
+                    <Button type="submit" variant="contained" className={classes.button}  onClick={getBill} >
+                      <b>Kiểm tra</b> <FindInPageOutlined className={classes.rightIcon}/>
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12} sm={1}></Grid>
+                  <Grid item xs={12} sm={12}>
+                    {error ? <p>{error}</p> : null}
+                  </Grid>
+              </Grid>
+            </Box>
           </Grid>
-        </React.Fragment>
+        </Grid>
     )
 }
