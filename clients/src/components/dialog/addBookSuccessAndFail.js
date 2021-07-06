c60edaf68823031db95f9daddb9103fc72bee4e4
nerrr <vietnhat.dev@gmail.com>
2021-07-07 00:26:48 +0700

    fix Alert

--- clients/src/components/dialog/addBookSuccessAndFail.js
+++ clients/src/components/dialog/addBookSuccessAndFail.js
@@ -1,72 +1,72 @@
-// import React from 'react';
-// import Button from '@material-ui/core/Button';
-// import Snackbar from '@material-ui/core/Snackbar';
-// import MuiAlert from '@material-ui/lab/Alert';
-// import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
-// function Alert(props) {
-//   return <MuiAlert elevation={6} variant="filled" {...props} />;
-// }
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: '100%',
-//     '& > * + *': {
-//       marginTop: theme.spacing(2),
-//     },
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
-// export  function SuccessSnackbars(props) {
-//   const {open,setOpen} = props
-//   const classes = useStyles();
+export  function SuccessSnackbars(props) {
+  const {open,setOpen} = props
+  const classes = useStyles();
 
-//   const handleClick = () => {
-//     setOpen(true);
-//   };
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-//   const handleClose = (event, reason) => {
-//     if (reason === 'clickaway') {
-//       return;
-//     }
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-//     setOpen(false);
-//   };
+    setOpen(false);
+  };
 
-//   return (
-//     <div className={classes.root}>
-//       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-//         <Alert onClose={handleClose} severity="info">
-//         Booking success
-//         </Alert>
-//       </Snackbar>
-//     </div>
-//   );
-// }
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="info">
+        Booking success
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
 
-// export  function FailSnackbars(props) {
-//   const {open,setOpen} = props
-//   const classes = useStyles();
+export  function FailSnackbars(props) {
+  const {open,setOpen} = props
+  const classes = useStyles();
 
-//   const handleClick = () => {
-//     setOpen(true);
-//   };
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-//   const handleClose = (event, reason) => {
-//     if (reason === 'clickaway') {
-//       return;
-//     }
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-//     setOpen(false);
-//   };
+    setOpen(false);
+  };
 
-//   return (
-//     <div className={classes.root}>
-//       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-//         <Alert onClose={handleClose} severity="warning">
-//         Booking Failed
-//         </Alert>
-//       </Snackbar>
-//     </div>
-//   );
-// }
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning">
+        Booking Failed
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
